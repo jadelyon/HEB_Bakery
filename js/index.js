bb79228bcab7a983f3206266fe59d68a41850244
jadelyon <67938728+jadelyon@users.noreply.github.com>
2020-12-22 20:19:41 -0600

    Images

--- js/index.js
+++ js/index.js
@@ -1,11 +1,1 @@
 
-
-// FIlter anything
-$(document).ready(function () {
-    $("#anythingSearch").on("keyup", function () {
-      var value = $(this).val().toLowerCase();
-      $("#myDIV *").filter(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    });
-  });
