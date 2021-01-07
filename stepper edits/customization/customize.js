c0bb586b0953d70757132b134689f033887e137e
jadelyon <67938728+jadelyon@users.noreply.github.com>
2021-01-06 18:58:30 -0600

    customization updates

--- stepper edits/customization/customize.js
+++ stepper edits/customization/customize.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
+
     var current_fs, next_fs, previous_fs; //fieldsets
     var opacity;
+    
     $(".next").click(function(){
+    
     current_fs = $(this).parent();
     next_fs = $(this).parent().next();
+    
     //Add Class Active
     $("#progressbar li").eq($("fieldset").index(current_fs)).addClass("active");
+    
     //show the next fieldset
     next_fs.show();
     //hide the current fieldset with style
@@ -13,6 +18,7 @@ $(document).ready(function(){
     step: function(now) {
     // for making fielset appear animation
     opacity = 1 - now;
+    
     current_fs.css({
     'display': 'none',
     'position': 'relative'
@@ -22,18 +28,24 @@ $(document).ready(function(){
     duration: 600
     });
     });
+    
     $(".previous").click(function(){
+    
     current_fs = $(this).parent();
     previous_fs = $(this).parent().prev();
+    
     //Remove class active
     $("#progressbar li").eq($("fieldset").index(previous_fs)).removeClass("active");
+    
     //show the previous fieldset
     previous_fs.show();
+    
     //hide the current fieldset with style
     current_fs.animate({opacity: 0}, {
     step: function(now) {
     // for making fielset appear animation
     opacity = 1 - now;
+    
     current_fs.css({
     'display': 'none',
     'position': 'relative'
@@ -43,11 +55,14 @@ $(document).ready(function(){
     duration: 600
     });
     });
+    
     $('.radio-group .radio').click(function(){
     $(this).parent().find('.radio').removeClass('selected');
     $(this).addClass('selected');
     });
+    
     $(".submit").click(function(){
     return false;
     })
+    
     });
